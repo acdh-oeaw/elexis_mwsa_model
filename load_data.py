9ae03223d3d04af2c8a09f3910de5bdcafc6e34f
lenkaB <lenka.bajcetic@gmail.com>
2020-02-11 11:43:40 +0100

    Add files via upload

--- load_data.py
+++ load_data.py
@@ -2,6 +2,7 @@ import csv
 import os
 import nltk
 from nltk.corpus import wordnet as wn
+import random
 
 folder = '/Users/lenka/Desktop/training_data'
 
@@ -38,8 +39,8 @@ def analyze_by_class(dataset):
             separated[class_value] = list()
         separated[class_value].append(vector[0])
 
-    for s in separated:
-        print(s, len(separated[s]))
+    #for s in separated:
+    #    print(s, len(separated[s]))
 
     return separated
 
@@ -58,7 +59,7 @@ def find_features(row):
 
     features['synsets'] = len(wn.synsets(row['lemma'])) #for specific pos e.g. wn.synsets('dog', pos=wn.VERB)
 
-    #if features['synsets'] == 0:
+    # if features['synsets'] == 0:
     #    print('no synset for ',row['lemma']) TODO MULTILIGNUAL WN
 
     return features
@@ -72,17 +73,67 @@ def prepare_data(dataset):
     return train_set, test_set
 
 
+def get_baseline(test_set):
+    TP = 0
+    for el in test_set:
+        if el[1]=='none':
+            TP+=1
+
+    return float(TP/len(test_set))
+
+
+def balance_classes(separated):
+    second_biggest_class = 0
+    for el in separated:
+        if el != 'none':
+          if len(separated[el])>second_biggest_class:
+              second_biggest_class = len(separated[el])
+
+    separated['none'] = separated['none'][:second_biggest_class]
+
+    balanced = []
+    for el in separated:
+        for row in separated[el]:
+            balanced.append((row, el))
+
+    random.shuffle(balanced)
+
+    return balanced
+
+
+def train_and_test_classifiers(data):
+    train_set, test_set = prepare_data(data)
+    naive_bayes = nltk.NaiveBayesClassifier.train(train_set)
+    print('naive bayes: ', nltk.classify.accuracy(naive_bayes, test_set))
+    #naive_bayes.show_most_informative_features(5)
+
+    decision_tree = nltk.DecisionTreeClassifier.train(train_set)
+    print('desicion tree: ', nltk.classify.accuracy(decision_tree, test_set))
+
+    print('baseline: ', str(get_baseline(test_set)) + '\n')
+
+    # quite slow
+    # max_ent = nltk.MaxentClassifier.train(train_set, trace=-1)
+    # print(nltk.classify.accuracy(max_ent, test_set))
+    # max_ent.show_most_informative_features(5)
+    # print('\n')
+
 
 if __name__ == '__main__':
 
     data = load_training_data(folder)
 
     for lang in data:
-        print(lang)
-        analyze_by_class(data[lang])
+        if lang=='english':
+            continue
 
-        train_set, test_set = prepare_data(data[lang])
-        classifier = nltk.NaiveBayesClassifier.train(train_set)
-        print(nltk.classify.accuracy(classifier, test_set))
-        classifier.show_most_informative_features(5)
-        print('\n')
+        print(lang)
+
+        separated = analyze_by_class(data[lang])
+        balanced = balance_classes(separated)
+
+        print('balanced dataset: ',len(balanced))
+        train_and_test_classifiers(balanced)
+
+        #print('whole dataset: ', len(data[lang]))
+        #train_and_test_classifiers(data[lang])
