9dc5f27527ccddee62bb86b7aa9a4d9e4ab75d8e
Yim <syim@oeaw.ads>
2020-02-17 17:16:22 +0100

    Play with hyperparameter grid search

--- codalab_ml.py
+++ codalab_ml.py
@@ -1,14 +1,19 @@
+import warnings
+
+from sklearn.svm import LinearSVC, SVC
+from sklearn.tree import DecisionTreeClassifier
+
+warnings.filterwarnings('ignore')
 import os
 import spacy
 import pandas as pd
-
+from pprint import pprint
 from datetime import datetime
 from sklearn import svm, preprocessing, tree
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import f1_score, classification_report, confusion_matrix
-from sklearn.model_selection import cross_val_score
-from sklearn.naive_bayes import ComplementNB, MultinomialNB
+from sklearn.model_selection import cross_val_score, GridSearchCV
 
 from load_data import split_data, difference_in_length, first_word_same, jaccard_sim, cosine
 from load_data import train_and_test_classifiers
@@ -26,6 +31,7 @@ def load_data(file_path):
     add_column_names(loaded_data)
 
     return loaded_data
+
 
 def extract_features(data, feats_to_scale):
     def sentence2vec(row):
@@ -70,7 +76,7 @@ def run_cv_with_dataset(model, trainset, y_train):
 
 
 def cross_val_models(models, x_train, y_train):
-    for estimator in models['unscaled']:
+    for estimator in models:
         run_cv_with_dataset(estimator, x_train, y_train)
 
     # for estimator in models['scaled']:
@@ -87,22 +93,71 @@ def get_baseline_df(y_test):
 
 
 def train_models_sklearn(x_train, y_train):
-    lr = LogisticRegression(solver='lbfgs', multi_class='multinomial').fit(x_train, y_train)
-    # lr_scaled = LogisticRegression( solver = 'lbfgs', multi_class = 'multinomial').fit(X_train_scaled, y_train)
+    lr = {'estimator': LogisticRegression(), 'parameters': {}}
+    svm_model = {
+        'estimator': SVC(),
+        'parameters': {
+            'C': [3, 5, 10],
+            'kernel': ['rbf', 'linear', 'poly', 'sigmoid']
+        }
+    }
+    rf = {
+        'estimator': RandomForestClassifier(),
+        'parameters': {
+            'bootstrap': [True],
+            'max_depth': [2, 3, 5, 7, 10],
+            'max_features': [2, 3],
+            'min_samples_leaf': [3, 4, 5],
+            'min_samples_split': [2, 5, 8, 10, 12],
+            'n_estimators': [50, 100, 200]
+        }
+    }
+    dt = {'estimator': DecisionTreeClassifier(), 'parameters': {}}
 
-    # Linear kernal won't work very well, experiment with nonlinear ones.
-    svm_model = svm.LinearSVC(C=1.0) .fit(x_train, y_train)
-    # svm_scaled = svm.LinearSVC().fit(X_train_scaled, y_train)
+    models = {'unscaled': [svm_model]}
 
-    rf = RandomForestClassifier(max_depth=10, random_state=0).fit(x_train, y_train)
-    # rf_scaled = RandomForestClassifier(max_depth = 5, random_state=0).fit(X_train_scaled, y_train)
+    tuned_models = tune_hyperparams(models, x_train, y_train)
 
-    dt = tree.DecisionTreeClassifier().fit(x_train, y_train)
+    return tuned_models
 
-    models = {'unscaled': [lr, svm_model, rf, dt]}
-    # models['scaled'] = [lr_scaled, svm_scaled, rf_scaled]
 
-    return models
+def tune_hyperparams(estimators, x_train, y_train):
+    result = []
+    for estimator in estimators['unscaled']:
+        params = estimator['parameters']
+
+        scores = ['precision', 'recall', 'f1']
+
+        for score in scores:
+            print("# Tuning hyper-parameters for %s" % score)
+            print()
+
+            grid_search = GridSearchCV(estimator=estimator['estimator'], param_grid=params,
+                                       scoring='%s_weighted' % score, cv=5,
+                                       n_jobs=-1, verbose=1)
+
+            print("Performing grid search...")
+            print("parameters:")
+            pprint(params)
+            grid_search.fit(x_train, y_train)
+            print()
+
+            means = grid_search.cv_results_['mean_test_score']
+            stds = grid_search.cv_results_['std_test_score']
+            report_file.write('Precision: \n')
+            for mean, std, parameters in zip(means, stds, grid_search.cv_results_['params']):
+                report_file.write("%0.3f (+/-%0.03f) for %r"
+                                  % (mean, std * 2, parameters) + '\n')
+
+            report_file.write("Best score: %0.3f" % grid_search.best_score_ + '\n')
+            report_file.write("Best parameters set:\n")
+            best_parameters = grid_search.best_estimator_.get_params()
+            for param_name in sorted(params.keys()):
+                report_file.write("\t%s: %r" % (param_name, best_parameters[param_name]) + '\n')
+
+            result.append(grid_search.best_estimator_)
+
+    return result
 
 
 def is_not_none(df):
@@ -125,7 +180,7 @@ def compare_on_testset(models, testset_x, testset_y, testset_x_scaled):
         report_file.write(str(classification_report(testset_y, predicted)) + '\n')
         report_file.write(str(confusion_matrix(testset_y, predicted)) + '\n')
 
-        report_file.write('\t\t' + "F-Score:" + str(f1_score(testset_y, predicted, average = 'macro')) + '\n')
+        report_file.write('\t\t' + "F-Score:" + str(f1_score(testset_y, predicted, average='weighted')) + '\n')
 
         score = estimator.score(testset_x, testset_y)
         report_file.write('\t\t' + "Accuracy: %0.4f (+/- %0.4f)" % (score.mean(), score.std() * 2) + '\n')
@@ -155,7 +210,7 @@ def load_and_preprocess():
         second_biggest = imbalanced_set.groupby('relation').count().word.sort_values(ascending=False)[1]
         balanced = imbalanced_set.drop(none.index[second_biggest:])
 
-        return balanced.sample(frac=1, random_state = 7)
+        return balanced.sample(frac=1, random_state=7)
 
     all_data = load_training_data()
     en_data = all_data['english_kd']
@@ -167,14 +222,14 @@ def load_and_preprocess():
     return balanced
 
 
-def train(data, with_testset = False):
+def train(data, with_testset=False):
     train_and_test_classifiers(data['nltk']['trainset'], data['nltk']['testset'])
     trained_models = train_models_sklearn(data['pd']['x_trainset'],
                                           data['pd']['y_trainset'])
     cross_val_models(trained_models, data['pd']['x_trainset'],
                      data['pd']['y_trainset'])
 
-    #compare_on_testset(trained_models, data['pd']['x_testset'],
+    # compare_on_testset(trained_models, data['pd']['x_testset'],
     #                  data['pd']['y_testset'], data['pd']['x_testset'])
 
 
@@ -198,6 +253,3 @@ if __name__ == '__main__':
     train(all_train_and_testset)
 
     report_file.close()
-
-
-
