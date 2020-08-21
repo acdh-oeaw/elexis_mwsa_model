d84efdf3eb75d08d9a0419965d5e6ba9de455a04
Seungbin Yim <liam21c@gmail.com>
2020-08-21 13:57:46 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='mwsa_pipeline',
+      version=1.0,
+      description='Elexis Monolingual Word Sense Disambiguation Pipeline',
+      author='Seung-bin Yim, Lenka Bajcetic',
+      author_email='seung-bin.yim@oeaw.ac.at',
+      url='https://github.com/acdh-oeaw/elexis_mwsa_model'
+      packages=['mwsa.transformers'])
