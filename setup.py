a33050ca2ec8af828ffdd4e9c3edb8da045efb67
Seungbin Yim <liam21c@gmail.com>
2020-08-21 15:09:59 +0200

    Modify setup.py

--- setup.py
+++ setup.py
@@ -6,5 +6,5 @@ setup(name='mwsa_pipeline',
       author='Seung-bin Yim, Lenka Bajcetic',
       author_email='seung-bin.yim@oeaw.ac.at',
       url='https://github.com/acdh-oeaw/elexis_mwsa_model',
-      packages=['mwsa.transformers'],
+      py_modules=['mwsa.transformers.pipeline'],
 )
